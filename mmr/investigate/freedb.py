73028881b7e89583481c93e8ffd26282976a342b
mathgl <mathgl67@gmail.com>
2010-03-17 16:02:23 +0100

    quick and dirty draft for the freedb plugin.

---
+++ mmr/investigate/freedb.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# vi:ai:et:ts=4 sw=4
+#
+# -*- coding: utf8 -*-
+#
+# PyMmr My Music Renamer
+# Copyright (C) 2007-2010  mathgl67@gmail.com
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+from mmr.config import Config
+from mmr.album import Album
+from mmr.track import Track
+from mmr.investigate.abstract_investigate import AbstractInvestigate
+
+# Display a fatal error when MySQLdb is not installed.
+try:
+    import MySQLdb
+except ImportError as exception:
+    print "FATAL: MySQLdb python module is require and must be installed. (python-mysql)"
+    import sys
+    sys.exit(1)
+
+
+class Investigate(AbstractInvestigate):
+    def _set_up_(self):
+        self.config = Config()
+        self.db = MySQLdb.connect(
+            host=self.config.values['freedb']['host'],
+            user=self.config.values['freedb']['user'],
+            passwd=self.config.values['freedb']['password'],
+            db=self.config.values['freedb']['db']
+        )
+        self._album_ = Album('freedb')
+
+    def do_album(self):
+        for res in self._album_list_:
+            if res.artist and res.album:
+                print "i have two!"
+
+                self.db.query("""
+                    SELECT genre, year FROM album WHERE artist LIKE "%s" AND title LIKE "%s"
+                """ % ( res.artist, res.album ))
+
+                r = self.db.store_result()
+                for (genre, year) in r.fetch_row(0):
+                    self._album_.artist = res.artist
+                    self._album_.album = res.album
+                    self._album_.genre = genre
+                    self._album_.year = year
+
+        return self._album_
+
+    def do_track(self, file_obj, result_array):
+        return None
+
